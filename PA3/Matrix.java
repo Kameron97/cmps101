69b4f9bb677e642869c24e9be44c157a38d11465
Kameron Gill <kgill2@Ucsc.edu>
2017-02-07 13:11:23 -0800

    worked on Matrix.java

--- PA3/Matrix.java
+++ PA3/Matrix.java
@@ -30,3 +30,55 @@ public class Matrix{
 
 	//Makes a new n x n zero Matrix. pre: n>=1
 	void Matrix(int n){
+		if(n <1){
+			throw new RuntimeException("Matrix error: Matrix not big enough!");
+		}
+		size = n;
+		row = new List[size];
+
+		for(int i = 0; i<size; i++){
+			row[i] = new List();
+		}
+	}
+
+	
+	//Access functions
+	
+	//returns n, the number of rows and column of this Matrix
+	int getSize(){
+		return size;
+	}
+
+	//returns the number of non-zero entries in this Matrix
+	int getNNZ(){
+		return NNZ;
+	}
+
+	public boolean equals(Object x){
+		Matrix temp = (Matrix) x;
+
+		if(getSize() != ((Matrix) x).getSize()){
+			return false;
+
+			for(int i = 0; i<=getSize(); i++){
+				if(!( row[i].equals((Matrix) x).row[i])){
+					return false;
+				}
+			}
+		}
+		return true
+	}
+
+	///Manipulation procedures
+
+	//sets this Matrix to the zero state
+	void makeZero(){
+		for(int i = 0; i<=getSize(); i++){
+			row[i].clear();
+		}
+		NNZ = 0;
+	}
+
+	//returns a new Matrix having the same entries as this Matrix
+	Matrix copy(){
+
